d4c5677d3e676cdda0f6d21a1b998f8f1fc38fb7
Pea75x <priya_93_x@hotmail.co.uk>
2020-11-13 17:03:06 +0000

    Add Forecast

--- src/script.js
+++ src/script.js
@@ -50,12 +50,34 @@ let h2 = document.querySelector("h2");
 h2.innerHTML= getHours(response.data.dt*1000);
 }
 
+function showCityForecast (response) {
+let forecastElement = document.querySelector(".forecast");
+forecastElement.innerHTML = null;
+let forecast = null; 
+
+
+for (let index = 0; index<5; index++) {
+forecast = response.data.list[index];
+forecastElement.innerHTML += `
+<div class="col"> 
+    <div class="more">
+        <h4> ${getHours(forecast.dt*1000)} </h4>
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" style="width: 50px;"/>
+        <p><strong>${Math.round(forecast.main.temp_max)}°</strong>/${Math.round(forecast.main.temp_min)}°</p>
+    </div>
+</div>`
+}}
+
 
 function search (city) {
 let apiKey = "c2016b2ed087ee4f9a30ab9ed51cf5fb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCityTemp);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showCityForecast);
+
 
 }
 
@@ -77,6 +99,9 @@ let apiKey = "c2016b2ed087ee4f9a30ab9ed51cf5fb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCityTemp);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showCityForecast);
 
 }
 
